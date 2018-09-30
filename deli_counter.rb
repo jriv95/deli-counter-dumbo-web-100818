1601a36dc6bca0cdb088c784b68e94a05bc81606
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-30 14:48:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,8 @@ def line(x)
   puts "The line is currently: #{line_array.join(" ")}"
   end
 end
+
+def take_a_number(katz_deli, name)
+    katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
