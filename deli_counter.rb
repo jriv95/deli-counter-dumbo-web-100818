1e02a8a92e2bb0d7615c3d8657a034063d7f462e
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-30 14:58:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,9 +16,9 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli, name)
+def now_serving(katz_deli)
   while katz_deli.length > 0
-    puts "Curently serving #{name}"
+    puts "Curently serving #{katz_deli[0]}"
     katz_deli.shift
   end
   puts "There is nobody waiting to be served."
