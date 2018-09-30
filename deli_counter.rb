f1c6fa34c13598ccfba58f9e6983f26db6a272f4
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-30 14:40:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+    puts The line is currently empty. if katz_deli.length == 0
+  x = 0
+  counter = "The line is currently: "
+  while x < katz_deli.length
+    counter += "#{x+1}. #{katz_deli[x]}"
+    x += 1
+  end
+end
+
+
+def line(x)
+  line_array = []
+  if x.length == 0
+    puts "The line is currently empty."
+  else x.each.with_index(1) do |name, index|
+    line_array.push("#{index}. #{name}")
+  end
+  puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
